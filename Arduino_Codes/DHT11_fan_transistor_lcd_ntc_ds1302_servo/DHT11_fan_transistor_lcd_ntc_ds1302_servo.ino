5233a3062e6b4feb2d2105957ad73c9564c87de7
emresagir <emresagir1310@gmail.com>
2021-02-28 18:56:38 +0300

    Ds1302_Servo_Added

--- Arduino_Codes/DHT11_fan_transistor_lcd_ntc_ds1302_servo/DHT11_fan_transistor_lcd_ntc_ds1302_servo.ino
+++ Arduino_Codes/DHT11_fan_transistor_lcd_ntc_ds1302_servo/DHT11_fan_transistor_lcd_ntc_ds1302_servo.ino
@@ -1,14 +1,19 @@
 #include <dht.h>
 #include <Wire.h> // Library for I2C communication
 #include <LiquidCrystal_I2C.h> // Library for LCD
+#include <virtuabotixRTC.h>
+#include <Servo.h>
 
 dht DHT;
 LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 16, 2);
+virtuabotixRTC myRTC(9,8,10);
+Servo servo;
 
 int dht11Pin = 4;
 int fanPin = 7;
 int thermistorPin = 0;
 int relayPin = 2;
+int servoPin = 3;
 
 double humid, temp, correctHumid, correctTemp;
 
@@ -16,6 +21,10 @@ int Vo;
 float R1 = 10000;
 float logR2, R2, T, Tc, Tf;
 float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
+int pos;
+int servoTime;
+int servoTimeControl;
+int servoFlag = 1;
 
 int del = 500;
 void setup() {
@@ -23,42 +32,51 @@ void setup() {
   pinMode(relayPin, OUTPUT);
   digitalWrite(fanPin, LOW);
   digitalWrite(relayPin, LOW);
+  servo.attach(servoPin);
+  
   Serial.begin(9600);
   lcd.init();
   lcd.backlight();
   Serial.println("Start");
+
+  servoTimeControl = myRTC.minutes;
+  //second, min, hour, day of the week, day of the month, month, year
+  //myRTC.setDS1302Time(00, 30, 17, 7, 28, 2, 2021); //Uncomment for recalibrating
 }
 
 void loop() {
+ timeFunc();
  
  int chk = DHT.read11(dht11Pin);
  
  humid = DHT.humidity;
  temp = DHT.temperature;
  
- //Accurate value detection
+ 
  humidFanControlFunc();
  
  tempFunc();
 
- 
  tempCheckRelay(Tc);
- //LCD part
+ 
  lcdFunc();
+
+ servoControlFunc();
 
  
  delay(del);
  
-
+ Serial.print("Servopos = ");
+ Serial.println(pos);
 }
 
 
 void humidFanControlFunc(){
 
   if (humid > 0 || temp > 0){
-         Serial.print(DHT.humidity);
+         /*Serial.print(DHT.humidity);
          Serial.print("humidity ---- temp");
-         Serial.println(DHT.temperature);
+         Serial.println(DHT.temperature);*/
          correctHumid = DHT.humidity; // We dont want to use -999 value
                                // because of the dht11's frequency 
                                //we can read 1 time in a second
@@ -92,6 +110,7 @@ void lcdFunc(){
   lcd.print(Tc);
 }
 
+
 void tempFunc(){
   Vo = analogRead(thermistorPin);
   R2 = R1 * (1023.0 / (float)Vo - 1.0);
@@ -100,11 +119,11 @@ void tempFunc(){
   Tc = T - 273.15;
   Tf = (Tc * 9.0)/ 5.0 + 32.0; 
 
-  Serial.print("Temperature: "); 
+  /*Serial.print("Temperature: "); 
   Serial.print(Tf);
   Serial.print(" F; ");
   Serial.print(Tc);
-  Serial.println(" C");
+  Serial.println(" C");*/
 }
 
 void tempCheckRelay(float temp){
@@ -114,4 +133,60 @@ void tempCheckRelay(float temp){
   if (temp < 37.5){
     digitalWrite(relayPin, HIGH);
   }
+}
+
+void timeFunc(){
+  myRTC.updateTime();
+  Serial.print("Current Date / Time: ");
+  Serial.print(myRTC.dayofmonth);
+  Serial.print("/");
+  Serial.print(myRTC.month);
+  Serial.print("/");
+  Serial.print(myRTC.year);
+  Serial.print("  ");
+  Serial.print(myRTC.hours);
+  Serial.print(":");
+  Serial.print(myRTC.minutes);
+  Serial.print(":");
+  Serial.println(myRTC.seconds);
+  
+  servoTime = myRTC.minutes;
+  Serial.print("ServoTime = ");
+  Serial.println(servoTime);
+  
+}
+
+void servoFunc(){
+  for (pos; pos < 145; pos++){
+    servo.write(pos);
+    Serial.println(pos);
+    delay(50);
+  }
+  
+}
+
+void servoReverseFunc(){
+  for (pos; pos > 25; pos--){
+    servo.write(pos);
+    Serial.println(pos);
+    delay(50);
+  }
+}
+
+void servoControlFunc(){
+  if (servoTime == servoTimeControl + 1){
+    if(servoFlag == -1){
+      servoFunc();
+    }
+    if(servoFlag == 1){
+      servoReverseFunc();
+    }
+    servoFlag = servoFlag * -1;// Changing rotation 
+    Serial.print("ServoFlag = ");
+    Serial.println(servoFlag);
+  }
+  servoTimeControl = servoTime;
+  Serial.print("ServoTimeControl = ");
+  Serial.println(servoTimeControl);
+  
 }
